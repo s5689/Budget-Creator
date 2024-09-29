c1b44c684ca42ef491241169d774582e386dc0f7
s5689 <lemirl0ug@gmail.com>
2024-09-29 13:09:46 -0300

    day 3

--- js/events.js
+++ js/events.js
@@ -2,6 +2,10 @@ import { db } from "./db";
 
 const searchBar = document.getElementById("searchBar");
 const resultsHTML = document.querySelector("#selectContainer table");
+const detailsHTML = document.getElementById("details");
+const detailsTableHTML = document.querySelector("#details tbody");
+
+const currentBudget = [];
 
 // Search Bar Event
 searchBar.addEventListener("keyup", () => {
@@ -51,26 +55,70 @@ searchBar.addEventListener("keyup", () => {
   });
 
   resultsHTML.innerHTML = txt;
+});
 
-  console.log(foundList);
-  console.log(searchBar.value);
+// Details Right Click
+document.addEventListener("contextmenu", (e) => {
+  const currentRow = e.srcElement.parentElement;
+
+  try {
+    // Proceder si el click fue dentro de la lista
+    if (checkClick(e)) {
+      e.preventDefault();
+      detailsHTML.setAttribute("show", "");
+
+      const rowID = currentRow.getAttribute("id");
+      let txt = "<tr>";
+
+      // Recorrer los datos seleccionados
+      Object.values(db[rowID]).forEach((value, k) => {
+        let tempValue = value;
+
+        // Dar formato a los Precios
+        if (k === 3 || k === 4) {
+          tempValue = toFormat(value);
+        }
+
+        txt += `<td>${tempValue}</td>`;
+      });
+
+      txt += "</tr>";
+
+      detailsTableHTML.innerHTML = txt;
+    }
+  } catch (error) {}
+});
+
+document.addEventListener("click", (e) => {
+  // Hide details
+  detailsHTML.removeAttribute("show");
+
+  if (checkClick(e)) {
+    const rowID = e.srcElement.parentElement.getAttribute("id");
+    console.log(rowID);
+  }
 });
 
 function toFormat(e) {
   const size = e.length;
   let result = "";
 
+  // Si no hay ningun valor, mostrar ---
   if (size === 0) {
     result = "---";
-  } else {
+  }
+  // De lo contrario, aplicar formato
+  else {
     let k = 0;
     let i = 0;
 
+    // Detectar donde aplicar el punto.
     while (true) {
       if (k !== 3) {
         result += e.charAt(size - i - 1);
 
         i++;
+        k++;
       } else {
         result += ".";
         k = 0;
@@ -79,18 +127,26 @@ function toFormat(e) {
       if (i === size) {
         break;
       }
-
-      k++;
     }
 
+    // Invertir el resultado
     let temp = "";
-
     for (let j = result.length - 1; j >= 0; j--) {
       temp += result[j];
     }
 
-    result = temp;
+    result = "$" + temp;
   }
 
   return result;
+}
+
+function checkClick(e) {
+  const currentRow = e.srcElement.parentElement;
+
+  if (currentRow.parentElement.parentElement.parentElement.getAttribute("id") === "selectContainer") {
+    return true;
+  }
+
+  return false;
 }
